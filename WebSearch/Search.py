d304925a47cbd3acfdbb5c42941b9d596f6755d0
vlgidenko <vlgidenko@gmail.com>
2022-08-26 20:28:18 -0400

    Adding web search

---
+++ WebSearch/Search.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://www.amazon.com')
+searchbox = driver.find_element(By.ID, "twotabsearchtextbox")
+searchbox.send_keys('potato')
+searchbox.submit()
+
+time.sleep(2)
+
+results = driver.find_elements(By.CSS_SELECTOR, ".s-result-item h2.a-size-mini")
+
+for result in results:
+    print(result.text)
+
+driver.quit()
